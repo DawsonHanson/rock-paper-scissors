06b77413e036a76a1f2b5518ca48d0ec3dc4acff
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-07-12 15:43:33 -0600

    Edit javascript.js File
    Added function to determine victor of a single round.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
 console.log ('test!')
 
+const playerSelection = playerSelect()
+const computerSelection = computerSelect()
+console.log (typeof playerSelection)
+console.log (typeof computerSelection)
+
 // input (prompt) from player to pick rock, paper, scissors 
+
 function playerSelect () {
   let question = prompt ('Rock Paper or Scissors?')
   let answer = question.toUpperCase ()
   return answer;
 }
-console.log (playerSelect ())
+console.log (typeof playerSelect)
+
 
 // function for computer to pick rock, paper, scissors at random 
 
@@ -16,9 +23,32 @@ function computerSelect () {
   let randomAnswer = array[randomArray];
   return randomAnswer;
 }
-console.log (computerSelect())
+console.log (typeof computerSelect)
 
 // function (if...elseif) to determine victor
+
+function playRound () {
+  if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+    alert ('You won! Rock beats Scissors')
+  }
+  else if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
+    alert ('You lost! Paper beats Rock')
+  } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+    alert ('You won! Paper beats Rock')
+  } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
+    alert ('You lost! Scissors beats Paper')
+  } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+    alert ('You won! Scissors beats Paper')
+  } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+    alert ('You lose! Rock beats Scissors')
+  } else {
+    alert ('Tie! no one wins')
+  }
+}
+console.log (playRound())
+console.log (playerSelection)
+console.log (computerSelection)
+
 // output (message) if player won or lost round
 // function/ loop to play a max of 5 rounds
 // output (message) if player won or lost game
