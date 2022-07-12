888f865c854b5f698ebf76e1e4e7dd8644e6298a
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-07-12 14:23:16 -0600

    Edit javascript.js File
    Added player and computer input functions.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,23 @@
 console.log ('test!')
+
 // input (prompt) from player to pick rock, paper, scissors 
+function playerSelect () {
+  let question = prompt ('Rock Paper or Scissors?')
+  let answer = question.toUpperCase ()
+  return answer;
+}
+console.log (playerSelect ())
+
 // function for computer to pick rock, paper, scissors at random 
+
+function computerSelect () {
+  let array = ['ROCK', 'PAPER', 'SCISSORS']
+  let randomArray = Math.floor((Math.random() *array.length));
+  let randomAnswer = array[randomArray];
+  return randomAnswer;
+}
+console.log (computerSelect())
+
 // function (if...elseif) to determine victor
 // output (message) if player won or lost round
 // function/ loop to play a max of 5 rounds
