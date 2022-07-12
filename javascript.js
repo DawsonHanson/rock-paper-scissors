f8119ccc12b5ec3bed8f2478698c07ea24ace42e
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-07-12 13:34:30 -0600

    Edit javascript.js File
    Added pseudocode template

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
 console.log ('test!')
+// input (prompt) from player to pick rock, paper, scissors 
+// function for computer to pick rock, paper, scissors at random 
+// function (if...elseif) to determine victor
+// output (message) if player won or lost round
+// function/ loop to play a max of 5 rounds
+// output (message) if player won or lost game
+// function to restart game when finished
