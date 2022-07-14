a1bdf91a184e27257aa624ff13fd1d83e099f169
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-07-14 11:47:48 -0600

    Edit javascript.js File
    Fixed bugs, now score is kept properly and game ends once the
    player / computer reaches 3 points.

--- javascript.js
+++ javascript.js
@@ -30,18 +30,25 @@ function playRound () {
   let answer = ''
   if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
     console.log ('You won! Rock beats Scissors')
+    answer = 'You won! Rock beats Scissors'
   } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
     console.log ('You lost! Paper beats Rock')
+    answer = 'You lost! Paper beats Rock'
   } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
     console.log ('You won! Paper beats Rock')
+    answer = 'You won! Paper beats Rock'
   } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
     console.log ('You lost! Scissors beats Paper')
+    answer = 'You lost! Scissors beats Paper'
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
     console.log ('You won! Scissors beats Paper')
+    answer = 'You lost! Scissors beats Paper'
   } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
     console.log ('You lost! Rock beats Scissors')
+    answer = 'You lost! Scissors beats Paper'
   } else {
     console.log ('Tie! no one wins')
+    answer = 'Tie! no one wins'
   }
   return answer;
 }
@@ -49,12 +56,15 @@ function playRound () {
 // function/ loop to play a max of 5 rounds
 
 function game () {
-  for (let x = 0; x < 5; x++) {
+  for (let x = 0; x < 100; x++) {
     gameScore()
     console.log (playerScore)
     console.log (computerScore)
     console.log (tie)
-  }
+    if (playerScore === 3 || computerScore === 3) {
+      break;
+    } 
+  } 
 }
 
 console.log (game())
@@ -62,12 +72,20 @@ console.log (game())
 // function to keep score and display message once game is done
 
 function gameScore () {
-  let answer = (playRound());
+  let answer = playRound()
   if (answer === 'You won! Rock beats Scissors' || answer === 'You won! Paper beats Rock' || answer === 'You won! Scissors beats Paper') {
     playerScore++;
   } else if (answer === 'You lost! Paper beats Rock' || answer === 'You lost! Scissors beats Paper' || answer === 'You lost! Rock beats Scissors') {
     computerScore++;
   } else {
     tie++;
+  } 
+
+  if (playerScore === 3) {
+    console.log ('Congratulations! You\'ve won!')
+  } 
+
+  if (computerScore === 3) {
+    console.log ('Computer won the game! if only you were smarter')
   } 
 }
