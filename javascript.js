eae6df33a3be4671d9bdd406450c6ccb4dc2ac2d
Dawson Hanson <Dawson.Hanson2@gmail.com>
2022-08-01 15:03:28 -0600

    Edit index.html & javascript.js File
    Rock paper scissors plays out in console using buttons.
    Need other functions to keep score and return a victor at 5 rounds.

--- javascript.js
+++ javascript.js
@@ -1,84 +1,60 @@
 console.log ('test!')
 
-let playerScore = 0
-let computerScore = 0
-let tie = 0
+let computer;
+let player;
 
-// input (prompt) from player to pick rock, paper, scissors 
+// player input through buttons
+const buttons = document.querySelectorAll('.button')
+buttons.forEach(button => button.addEventListener('click', () => {
+  player = Number(button.accessKey);
+  console.log(player)
+  console.log(typeof player)
+}))
 
-function playerSelect () {
-  let question = prompt ('Rock Paper or Scissors?')
-  let answer = question.toUpperCase ()
-  return answer;
-}
-
-// function for computer to pick rock, paper, scissors at random 
-
-function computerSelect () {
-  let array = ['ROCK', 'PAPER', 'SCISSORS']
-  let randomArray = Math.floor((Math.random() *array.length));
-  let randomAnswer = array[randomArray];
-  return randomAnswer;
-}
-
-// function (if...elseif) to determine victor
-
-function playRound () {
-  const playerSelection = playerSelect()
-  const computerSelection = computerSelect()
-  console.log(computerSelection)
-  let answer = ''
-  if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-    answer = 'You won! Rock beats Scissors'
-  } else if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-    answer = 'You lost! Paper beats Rock'
-  } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-    answer = 'You won! Paper beats Rock'
-  } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-    answer = 'You lost! Scissors beats Paper'
-  } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-    answer = 'You lost! Scissors beats Paper'
-  } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-    answer = 'You lost! Scissors beats Paper'
-  } else {
-    answer = 'Tie! no one wins'
-  }
-  return answer;
-}
-
-// function/ loop to play a game until player/computer reach 3 points
-
-function game () {
-  for (let x = 0; x < 100; x++) {
-    gameScore()
-    console.log (playerScore)
-    console.log (computerScore)
-    console.log (tie)
-    if (playerScore === 3 || computerScore === 3) {
+// function for computer input
+function computerInput() {
+  let randNum = Math.floor((Math.random() * 3) + 1)
+  
+  switch(randNum) {
+    case 1:
+      computer = 1
       break;
-    } 
-  } 
+    case 2:
+      computer = 2
+      break;
+    case 3:
+      computer = 3
+      break;
+  }
 }
 
-console.log ()
+// play a round and use result in another function
+const playRoundButton = document.querySelector('.play-round-button')
 
-// function to keep score and display message once game is done
+playRoundButton.addEventListener('click', () => {
+  computerInput()
+  console.log(computer)
+  console.log(typeof computer)
+  result();
+})
 
-function gameScore () {
-  let answer = playRound()
-  if (answer === 'You won! Rock beats Scissors' || answer === 'You won! Paper beats Rock' || answer === 'You won! Scissors beats Paper') {
-    playerScore++;
-  } else if (answer === 'You lost! Paper beats Rock' || answer === 'You lost! Scissors beats Paper' || answer === 'You lost! Rock beats Scissors') {
-    computerScore++;
+function result() {
+  let answer = ''
+  if (player == 1 && computer == 3) {
+    answer = 'Victory! Rock Beats Scissors'
+  } else if (player == 1 && computer == 2) {
+    answer = 'Defeat! Paper Beats Rock'
+  } else if (player == 2 && computer == 1) {
+    answer = 'Victory! Paper Beats Rock'
+  } else if (player == 2 && computer == 3) {
+    answer = 'Defeat! Scissors Beats Paper'
+  } else if (player == 3 && computer == 2) {
+    answer = 'Victory! Scissors Beats Paper'
+  } else if (player == 3 && computer == 1) {
+    answer = 'Defeat! Rock Beats Scissors'
   } else {
-    tie++;
-  } 
-
-  if (playerScore === 3) {
-    console.log ('Congratulations! You\'ve won!')
-  } 
-
-  if (computerScore === 3) {
-    console.log ('Computer won the game! if only you were smarter')
-  } 
+    answer = 'Draw!'
+  }
+  console.log(answer)
+  return answer;
 }
